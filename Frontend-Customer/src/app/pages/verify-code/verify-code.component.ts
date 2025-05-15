51987c94e1f5ce04fc73838c59af4e6280d3d88a
achraf048 <achrafchouki048@gmail.com>
2025-05-15 14:42:16 +0100

    Fix Commit

--- Frontend-Customer/src/app/pages/verify-code/verify-code.component.ts
+++ Frontend-Customer/src/app/pages/verify-code/verify-code.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-verify-code',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './verify-code.component.html',
   styleUrls: ['./verify-code.component.css']
 })
